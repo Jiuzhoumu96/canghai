155ca96fa079a7683e851271a400142360102a92
hechengyao <hechengyao@bonc.com.cn>
2021-08-06 17:15:17 +0800

    Commit:   通过配置类，使用以下配置文件的属性

--- src/main/java/com/lanhuigu/canghai/controller/PersonController.java
+++ src/main/java/com/lanhuigu/canghai/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.lanhuigu.canghai.controller;
 
+import com.lanhuigu.canghai.config.ApplicationConfig;
 import com.lanhuigu.canghai.po.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: HeChengyao
  * @date: 2021/7/29 11:45
  */
+// @Slf4j
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private Person person;
+    @Autowired
+    private ApplicationConfig applicationConfig;
 
     @GetMapping("/find")
-    public Person findPerson(){
+    public Person findPerson() {
+        String cs = applicationConfig.getCanHaiSangTian();
+        String rd = applicationConfig.getRenJanZengDao();
+        System.out.println(cs + rd);
+        // log.info(">>>>>>" + cs + "," + rd);
         return person;
     }
 
